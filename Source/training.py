4c2d6607f441c4a129092fc74773e043f7f2951c
koolone <38150137+koolone@users.noreply.github.com>
2019-02-26 00:13:31 -0500

    Update training.py
    update model training instance type

--- Source/training.py
+++ Source/training.py
@@ -74,7 +74,7 @@ training_params = \
     },
     "ResourceConfig": {
         "InstanceCount": 1,
-        "InstanceType": "ml.t2.large",
+        "InstanceType": "ml.m4.xlarge",
         "VolumeSizeInGB": 50
     },
     "TrainingJobName": job_name,
